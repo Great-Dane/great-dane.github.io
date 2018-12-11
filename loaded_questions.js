1f47cf8c37a21f7c687804023ca3ca17b4d758c6
Gurdane Sethi <gurdanesethi@gatech.edu>
2018-12-10 18:09:29 -0600

    Added styling and all buttons.

--- loaded_questions.js
+++ loaded_questions.js
@@ -1,6 +1,7 @@
 console.log('here we go');
+var idx = -1;
 var data;
-past_questions = [];
+var past_questions = [-1];
 
 Papa.parse("https://raw.githubusercontent.com/Great-Dane/great-dane.github.io/master/loaded_questions.csv", { 
     download: true,
@@ -10,10 +11,47 @@ Papa.parse("https://raw.githubusercontent.com/Great-Dane/great-dane.github.io/ma
     }
 });
 
-function anyQuestion() {
-    idx = Math.floor(Math.random()*477);
-    if(!past_questions.includes(idx)) {
-        past_questions.push(idx);
-        console.log(data[idx]);
+function anythingGoes() {
+    while (past_questions.includes(idx)) {
+        idx = Math.floor(Math.random()*115) + 120;
     }
+    past_questions.push(idx);
+    document.getElementById("question").innerHTML = data[idx][0];
+    document.getElementById("category").innerHTML = data[idx][1];
+}
+
+function hypotheticals() {
+    while (past_questions.includes(idx)) {
+        idx = Math.floor(Math.random()*120);
+    }
+    past_questions.push(idx);
+    document.getElementById("question").innerHTML = data[idx][0];
+    document.getElementById("category").innerHTML = data[idx][1];
+}
+
+function noBrainers() {
+    while (past_questions.includes(idx)) {
+        idx = Math.floor(Math.random()*119) + 237;
+    }
+    past_questions.push(idx);
+    document.getElementById("question").innerHTML = data[idx][0];
+    document.getElementById("category").innerHTML = data[idx][1];
+}
+
+function personals() {
+    while (past_questions.includes(idx)) {
+        idx = Math.floor(Math.random()*119) + 357;
+    }
+    past_questions.push(idx);
+    document.getElementById("question").innerHTML = data[idx][0];
+    document.getElementById("category").innerHTML = data[idx][1];
+}
+
+function randomQuestion() {
+    while (past_questions.includes(idx)) {
+        idx = Math.floor(Math.random()*477);
+    }
+    past_questions.push(idx);
+    document.getElementById("question").innerHTML = data[idx][0];
+    document.getElementById("category").innerHTML = data[idx][1];
 }
